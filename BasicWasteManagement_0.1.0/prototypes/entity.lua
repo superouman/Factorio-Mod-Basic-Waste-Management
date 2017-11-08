e93a2e4d9580d6df5040e42309bf6ccca718c288
superouman <philippe.cwik@gmail.com>
2017-11-08 12:49:38 +0100

    Changing how recipes are edited
    - Iterating directly on data.raw.recipe instead of having huge tables of
    item names
    - Reactivated the second slot in stone furnaces

--- BasicWasteManagement_0.1.0/prototypes/entity.lua
+++ BasicWasteManagement_0.1.0/prototypes/entity.lua
@@ -1,5 +1,6 @@
 --entity.lua
 
---data.raw["furnace"]["stone-furnace"]["result_inventory_size"] = 2
+-- Adds a slot for furnaces for solid waste
+data.raw["furnace"]["stone-furnace"]["result_inventory_size"] = 2
 data.raw["furnace"]["steel-furnace"]["result_inventory_size"] = 2
 data.raw["furnace"]["electric-furnace"]["result_inventory_size"] = 2
