f26b85c1cfe6f9eda9453403be36ee4af7452193
superouman <philippe.cwik@gmail.com>
2017-11-20 11:32:20 +0100

    Disabled stone furnace waste again

--- BasicWasteManagement_0.1.0/prototypes/entity.lua
+++ BasicWasteManagement_0.1.0/prototypes/entity.lua
@@ -1,6 +1,6 @@
 --entity.lua
 
 -- Adds a slot for furnaces for solid waste
-data.raw["furnace"]["stone-furnace"]["result_inventory_size"] = 2
+-- data.raw["furnace"]["stone-furnace"]["result_inventory_size"] = 2
 data.raw["furnace"]["steel-furnace"]["result_inventory_size"] = 2
 data.raw["furnace"]["electric-furnace"]["result_inventory_size"] = 2
